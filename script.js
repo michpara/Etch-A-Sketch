6b532cf1e237ac95436b82650389981b0536af0d
Michelle Paradis <46390671+michpara@users.noreply.github.com>
2019-05-31 16:54:39 -0400

    final minor fixes

--- script.js
+++ script.js
@@ -1,34 +1,52 @@
+
 //initial variables
-let gridSize = 100;
 let container = document.getElementById("container");
 let clear = document.getElementById("clear");
+let color = document.getElementById("color");
+let size = document.getElementById("size");
 let divs = document.getElementsByClassName("div");
+let toggle = false;
+let gridSize = 10;
 
+//clears the grid when the Clear Grid button is clicked
 clear.addEventListener("click", gridClear);
 
-//generates random colors for the grid
-function random_rgba() {
-    var o = Math.round, r = Math.random, s = 255;
-    return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
-}
+//changes the size of the grid when the button Change Size is clicked
+size.addEventListener("click", changeSize);
+
+//changes the color used when the Change Color button is clicked
+color.addEventListener("click", changeColor);
 
 //creates grid and allows it to be colored in.
-function createGrid(gridSize){
-	for(let i = 0; i < gridSize; i++){
+function createGrid(gridSize, toggle){
+	let powGridSize = Math.pow(gridSize, 2);
+	for(let i = 0; i < powGridSize; i++){
 
 		let gridDiv = document.createElement("div");
 
 		gridDiv.classList.add("div");
-		container.appendChild(gridDiv)
+		container.appendChild(gridDiv);
 
-		gridDiv.style.width = 400/Math.sqrt(gridSize);
-		gridDiv.style.height = 400/Math.sqrt(gridSize);
+		gridDiv.style.width = 400/gridSize;
+		gridDiv.style.height = 400/gridSize;
 	}
 
 	for(let i = 0; i < divs.length; i++){
 		divs[i].addEventListener("mouseover", function(){
-			divs[i].style.background = random_rgba();
+			if(!toggle){
+				divs[i].style.background = 'black';
+			}
+			else{
+				divs[i].style.background = random_rgba();
+			}
 		})
+	}
+}
+
+//clears the entire grid
+function gridClear(){
+	for(let i = 0; i<divs.length; i++){
+		divs[i].style.background = "";
 	}
 }
 
@@ -39,22 +57,25 @@ function removeGrid(){
 	}
 }
 
-//function to clear the entire grid when the button "clear" is clicked
-function gridClear(){
-	for(let i = 0; i<divs.length; i++){
-		divs[i].style.background = "";
-	}
-
-	gridSize = prompt("How many squares would you like per side for your new grid?")
-	
-	if (gridSize === ""){
-		gridSize = 100;
-	}
-	
-	removeGrid()
-	createGrid(gridSize)
+//changes the size of the grid
+function changeSize(){
+	gridSize = prompt("How many squares do you want per side?");
+	removeGrid();
+	createGrid(gridSize, toggle);
 }
 
-//function calls
-createGrid(gridSize)
-colorGrid()
+//changes the color of the grid
+function changeColor(){
+	removeGrid();
+	createGrid(gridSize, !toggle)
+	toggle = !toggle
+}
+
+//generates random colors for the grid
+function random_rgba() {
+    var o = Math.round, r = Math.random, s = 255;
+    return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
+}
+
+//creates the grid when the screen loads for the first time
+createGrid(10, false);
